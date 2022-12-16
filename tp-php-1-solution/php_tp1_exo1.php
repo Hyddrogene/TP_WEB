f34cc7142503c7dceb02d7b81690ad7d5b59834e
Corentin <Corentin.Behuet@univ-angers.fr>
2022-12-16 09:15:00 +0100

    HHHHH

---
+++ tp-php-1-solution/php_tp1_exo1.php
@@ -0,0 +1,3 @@
+<?php
+echo "<!DOCTYPE html>\n<html lang=\"fr\">\n<head>\n<meta charset=\"UTF-8\"/>\n<title>Super le titre</title>\n</head><body><hr/><h1>EN-TETE</h1><h2>Exemple de h2</h2><p>Type de balsie p<p><p>Bonjour le monde<p></body>";
+?>
